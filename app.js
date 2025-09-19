66c5561f598c2ad76ce6912a7fdc8a459202016f
Georgy Binu <geo.binu.25@gmail.com>
2025-09-19 08:19:44 +0530

    initial-commit

---
+++ app.js
@@ -0,0 +1,76 @@
+let unitsActive = false;
+let navUnitBtn = document.getElementById("drop-btn-unit");
+let unitsDropActive = document.getElementById("units-drop-active");
+const weatherPanel = document.getElementById("weather-panel");
+const wpLoader = document.getElementById("loading-weather-panel");
+const wpLocation = document.getElementById("wp-location");
+const wpDate = document.getElementById("wp-date");
+const wpTemp = document.getElementById("wp-temp");
+const wpFeels = document.getElementById("wp-feelslike");
+const wpHumidity = document.getElementById("wp-humidity");
+const wpWind = document.getElementById("wp-wind");
+const wpPreci = document.getElementById("wp-precipitation");
+
+navUnitBtn.addEventListener("click", () => {
+  console.log("clicked");
+  unitsDropActive.classList.toggle("hidden");
+  unitsActive = !unitsActive; //altering the value
+});
+const today = new Date();
+
+const formattedDate = today.toLocaleDateString("en-US", {
+  weekday: "long", // "Tuesday"
+  month: "short", // "Aug"
+  day: "numeric", // "5"
+  year: "numeric", // "2025"
+});
+async function setWeather(lat, long) {
+  try {
+    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,apparent_temperature,precipitation,relative_humidity_2m,weathercode,windspeed_10m,winddirection_10m&hourly=temperature_2m,weathercode&daily=temperature_2m_max,temperature_2m_min,weathercode&forecast_days=7&timezone=auto`;
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      if (response.status === 404) throw new Error("Data not found");
+      else if (response.status === 500) throw new Error("Server Error");
+      else throw new Error("Network was not okay");
+    }
+    const data = await response.json();
+    const current = data.current;
+    wpTemp.innerHTML = `${current.temperature_2m}&deg`;
+    wpFeels.innerHTML = `${current.apparent_temperature} &deg`;
+    wpHumidity.innerHTML = `${current.relative_humidity_2m} %`;
+    wpPreci.innerHTML = `${current.precipitation} mm`;
+    wpWind.innerHTML = `${current.windspeed_10m} km/h`;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
+async function getGeo() {
+  try {
+    const response = await fetch("http://ip-api.com/json/");
+
+    if (!response.ok) {
+      if (response.status === 404) throw new Error("Data not found");
+      else if (response.status === 500) throw new Error("Server Error");
+      else throw new Error("Network was not okay");
+    }
+    const data = await response.json();
+    if (data.status !== "success") {
+      throw new Error(`API error: ${data.message || "Unknown error"}`);
+    }
+    console.log("Geo data:", data);
+    await setWeather(data.lat, data.lon);
+    weatherPanel.classList.remove("hidden");
+
+    wpLoader.classList.add("hidden");
+    wpDate.textContent = formattedDate;
+    wpLocation.textContent = `${data.city}, ${data.country}`;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
+
+getGeo();
+
+console.log("App Running");
---
+++ style-guide.md
@@ -0,0 +1,30 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+> 💡 These are just the design sizes. Ensure content is responsive and meets WCAG requirements by testing the full range of screen sizes from 320px to large screens.
+
+## Colors
+
+### Neutral
+
+## Typography
+
+### Body Copy
+
+- Font size: 18px
+
+### Font
+
+- Family: [DM Sans](https://fonts.google.com/specimen/DM+Sans)
+- Weights: 300, 500, 600, 600i, 700
+
+- Family: [Bricolage Grotesque](https://fonts.google.com/specimen/Bricolage+Grotesque)
+- Weights: 700
+
+> 💎 [Upgrade to Pro](https://www.frontendmentor.io/pro?ref=style-guide) for design file access to see all design details and get hands-on experience using a professional workflow with tools like Figma. The design file for this challenge also includes a design system and tablet layout to help you build a more accurate solution faster.
